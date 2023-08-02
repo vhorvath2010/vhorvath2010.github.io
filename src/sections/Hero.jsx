2421b43933655e9ba39ca516089617beacc6533f
Vincent Horvath <vhorvath2010@gmail.com>
2023-08-01 23:44:52 -0500

    add dark mode support

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,10 @@
 function Hero() {
   return (
-    <section className="text-center min-h-screen justify-center flex-col flex items-center align-middle bg-yellow-50">
-      <h1 className="mx-4 mb-4 text-4xl md:text-6xl bg-slate-950 rounded-full text-yellow-50 p-6">
+    <section className="text-center min-h-screen justify-center flex-col flex items-center align-middle bg-yellow-50 dark:bg-slate-950">
+      <h1 className="mx-4 mb-4 text-4xl md:text-6xl bg-slate-950 rounded-full text-yellow-50 p-6 dark:bg-yellow-50 dark:text-slate-950">
         Vincent Horvath
       </h1>
-      <h2 className="mx-4 text-xl md:text-3xl text-slate-950">
+      <h2 className="mx-4 text-xl md:text-3xl text-slate-950 dark:text-yellow-50">
         Welcome to my portfolio website!
       </h2>
     </section>
