e8dcf7a4ae0d7ca99aa0056ded251f95eb503825
Vincent Horvath <vhorvath2010@gmail.com>
2023-08-01 23:41:30 -0500

    add basic hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <section className="text-center min-h-screen justify-center flex-col flex items-center align-middle bg-yellow-50">
+      <h1 className="mx-4 mb-4 text-4xl md:text-6xl bg-slate-950 rounded-full text-yellow-50 p-6">
+        Vincent Horvath
+      </h1>
+      <h2 className="mx-4 text-xl md:text-3xl text-slate-950">
+        Welcome to my portfolio website!
+      </h2>
+    </section>
+  );
+}
+
+export default Hero;
